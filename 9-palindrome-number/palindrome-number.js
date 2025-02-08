19fadd11b59dad3b72399c723d6518fe4e294930
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2025-02-08 13:19:42 +0700

    Time: 6 ms (79.59%) | Memory: 58.4 MB (69.69%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x <0 || (x %10===0 && x !== 00)) { 
+        return false;
+    }
+    let reversed = 0; 
+    let original = x; 
+
+    while (x > 0) { 
+        reversed = reversed *10 + (x % 10); 
+        x = Math.floor(x / 10); 
+    }
+    return reversed === original; 
+}; 
+console.log(isPalindrome(121)); 
+console.log(isPalindrome(-121)); 
+console.log(isPalindrome(10)); 
+    
+
