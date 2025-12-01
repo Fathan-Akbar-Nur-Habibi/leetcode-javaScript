6ca7bc4683cedf3bb35292643a4524014b7d9d2f
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2025-12-01 20:54:43 +0700

    Time: 67 ms (35.69%), Space: 60.9 MB (96.06%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) {
+        return null;
+    }
+    let pointerA = headA;
+    let pointerB = headB;
+    while (pointerA !== pointerB) {
+        if (pointerA === null) {
+            pointerA = headB;
+        } else {
+            pointerA = pointerA.next;
+        }
+        if (pointerB === null) {
+            pointerB = headA;
+        } else {
+            pointerB = pointerB.next;
+        }
+    }
+    return pointerA;
+};
