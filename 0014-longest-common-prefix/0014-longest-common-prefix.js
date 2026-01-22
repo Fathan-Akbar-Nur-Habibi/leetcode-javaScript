fc583adb019a3816ceb2d09908b5f41100f9735a
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2026-01-22 21:48:25 +0700

    Time: 0 ms (100%), Space: 53.7 MB (89.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -2,18 +2,22 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {
-    if (strs.length === 0) {
-        return "";
-    }
-    let prefix = strs[0];
-    for (let i = 1; i < strs.length; i++) {
-        const currentString = strs[i];
-        while (currentString.indexOf(prefix) != 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-            if (prefix.length === 0) {
-                return "";
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    let prefix = "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j][i] !== char) {
+                return prefix;
             }
         }
-    } return prefix;
+
+        prefix += char;
+    }
+
+    return prefix;
 };
