c0d0ca5674a518e3f1d12ae666e5b849dc1ad0cd
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2026-01-27 18:01:36 +0700

    Time: 1 ms (74.53%), Space: 60.7 MB (35.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  let insertPos = 0; 
-  for (let i = 0; i<nums.length; i++) { 
-    if (nums[i] !== 0) { 
-        let temp = nums[insertPos]; 
-        nums[insertPos] = nums[i]; 
-        nums[i] = temp; 
+    let insertPos = 0; 
+
+    //pindahkan  semua angka non-zero ke depan
+    for (let i = 0; i< nums.length; i++) { 
+        if(nums[i] !== 0){ 
+            nums[insertPos] = nums[i]; 
+            insertPos++;
+        }
+    }
+    // isi sisa array dengan 0
+    while (insertPos < nums.length) { 
+        nums[insertPos] = 0;
         insertPos++;
     }
-  }  
 };
