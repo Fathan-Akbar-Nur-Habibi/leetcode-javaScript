f794700cee9c85395c7d33335e55e94c6ac89dd8
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2026-01-28 11:29:27 +0700

    Time: 9 ms (37.39%), Space: 60.9 MB (97.97%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -2,12 +2,10 @@
  * @param {number[]} nums
  */
 var NumArray = function(nums) {
-    // Array prefix sum
-    this.prefix = [0];
-
-    // Menghitung prefix sum
-    for (let i = 0; i < nums.length; i++) {
-        this.prefix[i + 1] = this.prefix[i] + nums[i];
+    this.prefix = []; 
+    this.prefix[0] = nums[0]; 
+    for (let i =1; i < nums.length; i++) { 
+        this.prefix[i] = this.prefix[i-1] + nums[i]; 
     }
 };
 
@@ -17,5 +15,14 @@ var NumArray = function(nums) {
  * @return {number}
  */
 NumArray.prototype.sumRange = function(left, right) {
-    return this.prefix[right + 1] - this.prefix[left];
+    if(left === 0) { 
+        return this.prefix[right]; 
+    }
+    return this.prefix[right] - this.prefix[left-1]; 
 };
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
