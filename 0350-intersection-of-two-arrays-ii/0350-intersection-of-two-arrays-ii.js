fec7b0f39dd2d0780dcaf808bc1dfdbcdec75aa7
Fathan Akbar Nur Habibi <fathanakbarnh@gmail.com>
2026-01-28 12:51:38 +0700

    Time: 1 ms (81.94%), Space: 55.2 MB (72.87%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,17 +4,32 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    let map = {}; 
-    let result = []; 
 
-    for (let i = 0; i< nums1.length; i++){
-        map[nums1[i]] = (map[nums1[i]]|| 0) +1; 
+    // 1. Object untuk menyimpan frekuensi nums1
+    const freqMap = {};
+
+    // 2. Hitung frekuensi setiap angka di nums1
+    for (let num of nums1) {
+        freqMap[num] = (freqMap[num] || 0) + 1;
     }
-    for (let i = 0; i < nums2.length; i++) { 
-        if (map[nums2[i]] > 0) { 
-            result.push(nums2[i]);
-            map[nums2[i]]--;
+
+    // 3. Array untuk menyimpan hasil intersection
+    const result = [];
+
+    // 4. Loop nums2
+    for (let num of nums2) {
+
+        // 5. Jika angka ada di freqMap dan masih tersedia
+        if (freqMap[num] > 0) {
+
+            // 6. Masukkan ke hasil
+            result.push(num);
+
+            // 7. Kurangi frekuensi
+            freqMap[num]--;
         }
     }
+
+    // 8. Return hasil
     return result;
 };
